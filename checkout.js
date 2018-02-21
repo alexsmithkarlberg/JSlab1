a37bb07e182b7195a15e246607b358887296414f
alexsmithkarlberg <alex.smith.karlberg.91@gmail.com>
2018-02-21 11:50:46 +0100

    finished the Completion task i was assigned

--- checkout.js
+++ checkout.js
@@ -2,16 +2,10 @@ var zippat = /^\d{3}\s?\d{2}$/;
 var phonepat = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/;
 var namepat = /^[A-Za-z\s]+$/;
 var mailpat = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+var addresspat = /^[A-z]+\s\d+/;
 
-var zipcode = document.querySelector('#zipcode');
-var firstname = document.querySelector('#firstname');
-var lastname = document.querySelector('#lastname');
-var city = document.querySelector('#city');
-var email = document.querySelector('#email');
-var phonenumber = document.querySelector('#phonenumber');
 var inputs = document.querySelectorAll('.myInputs');
 var errors = document.querySelectorAll('.error');
-
 
 
 var bool = true;
@@ -19,11 +13,11 @@ var bool = true;
 document.querySelector('button[type="button"]').addEventListener('click', function(){
     bool = true;
     for (var i =0; i<inputs.length; i++) {
-        console.log(i);
        for (var j=0; j<errors.length; j++) {
            errors[i].innerHTML = "";
            errors[i].style.color = "red";
        }
+       console.log(inputs[i]);
 
         if (!mailpat.test(inputs[2].value)) {
             document.querySelector('#mailerror').innerHTML = "Use correct mail pattern";
@@ -31,6 +25,10 @@ document.querySelector('button[type="button"]').addEventListener('click', functi
         }
         if (!phonepat.test(inputs[3].value)) {
             document.querySelector('#phoneerror').innerHTML = "use swedish mobile format ex. xxxxxxxxxx";
+            bool = false;
+        }
+        if (!addresspat.test(inputs[4].value)) {
+            document.querySelector('#adresserror').innerHTML = "use correct street address format, ex. blablabla 24";
             bool = false;
         }
         if (!zippat.test(inputs[5].value)) {
